956f77199930959a0aa3bc7b6aa9c339f159102e
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-10-22 15:17:34 -0600

    new logic and files

--- liri.js
+++ liri.js
@@ -1,5 +1,43 @@
 require("dotenv").config();
-
-var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+//var command = process.argv[2];
+//var userInput = process.argv[3];
+
+
+function spotifySearch() {
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+} function movieSearch() {
+    axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=Trilogy&s=Batman").then(function (data, err) {
+        console.log(data.data);
+    }).catch(function (err) {
+        console.log(err);
+    })
+
+
+
+
+}
+
+movieSearch();
+function bands() {
+var artist = process.argv[2];
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (data, err) {
+console.log(data.data);
+    }).catch(function(err){
+        console.log(err);
+    })
+
+
+}
+bands();
+
 
