36156c6248b6ca6441d83fd4347c9438a1f4b4e7
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-10-23 20:24:53 -0600

    song search function is in jus need search and logic for choosing command

--- liri.js
+++ liri.js
@@ -3,33 +3,45 @@ var Spotify = require('node-spotify-api');
 var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
-//var command = process.argv[2];
+var command = process.argv[2];
 //var userInput = process.argv[3];
 
 
 function spotifySearch() {
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, response) {
+    spotify.search({ type: 'track', query: command, limit: "2", market: "US" }, function (err, response) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        var spotifyArray = Object.values(response);
+        for (var i = 0; i < spotifyArray.length; i++) {
+            // This logs song name
+            console.log(spotifyArray[i].items[i].name);
+            //This logs artist name
+            console.log(spotifyArray[i].items[i].artists[i].name);
+            // This logs the spotify url
+            console.log(spotifyArray[i].items[i].external_urls);
+            // This logs Album name
+            console.log(spotifyArray[i].items[i].album.name);
+        }
 
-       // console.log(response.data);
     });
-} function movieSearch() {
+
+}
+spotifySearch();
+function movieSearch() {
     axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=Trilogy&t=Batman-Begins&r=json").then(function (response, err) {
-    console.log(JSON.stringify(response.data.Title))  
-    console.log(JSON.stringify(response.data.Year));
-    console.log(JSON.stringify(response.data.Ratings))
-    console.log(JSON.stringify(response.data.Country));
-    console.log(JSON.stringify(response.data.Language));
-    console.log(JSON.stringify(response.data.Plot));
-    console.log(JSON.stringify(response.data.Actors));
-    //console.log(JSON.stringify(response.data))
-        
+        console.log(JSON.stringify(response.data.Title))
+        console.log(JSON.stringify(response.data.Year));
+        console.log(JSON.stringify(response.data.Ratings))
+        console.log(JSON.stringify(response.data.Country));
+        console.log(JSON.stringify(response.data.Language));
+        console.log(JSON.stringify(response.data.Plot));
+        console.log(JSON.stringify(response.data.Actors));
+
+
     }).catch(function (err) {
         console.log(err);
     })
-
 
 
 
@@ -37,17 +49,17 @@ function spotifySearch() {
 
 //movieSearch();
 function bands() {
-var artist = process.argv[2];
+
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response, err) {
-    console.log(JSON.stringify(response.data.name));
-    //console.log(response.data);
-    }).catch(function(err){
+       
+        //console.log(response.data);
+    }).catch(function (err) {
         console.log(err);
     })
 
 
 }
-bands();
+//bands();
 
 
 
